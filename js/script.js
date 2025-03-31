7086ac3ced7966b8a32a5552b156a9bf7810dc5c
Facundo Fracchia <facundofracchia98@hotmail.com>
2025-03-31 19:34:40 -0300

    0.05

--- js/script.js
+++ js/script.js
@@ -95,3 +95,8 @@ function mostrarResultado(destino, dias, personas, costoTotal) {
         <p>Costo total para ${personas} personas durante ${dias} días: <strong>$${costoTotal}</strong></p>
     `;
 }
+fetch("http://localhost:3000/destinos")
+  .then(response => response.json())
+  .then(destinos => console.log(destinos))
+  .catch(error => console.error("Error al cargar destinos:", error));
+  
